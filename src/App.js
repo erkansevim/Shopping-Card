b592785560cfac57a0c7095cc6db563a370e1475
erkansevim <erkansevim.essgmail.com>
2024-07-29 13:15:47 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import AppRouter from "./router/AppRouter"
+
+// index.html ye bootstrap in script teşekkürü ile fontAwesome ın teşekkür link ini yapıştırdık
+function App() {
+  return (
+    <div>
+    
+     <AppRouter/>
+    </div>
+  );
+}
+
+export default App;
